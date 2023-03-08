d2b2d7e3c9317775872e8aad0e16af3689c1aae7
Kolia <gaaved@gmail.com>
2023-03-08 20:03:33 +0200

    lesson 9-10

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\V1\UserController;
+
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/users')->name('users.')->group(function () {
+    Route::get('/sign-up', [UserController::class, 'signUp'])->name('sign-up');
 });
